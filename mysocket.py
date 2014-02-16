5d4b4a42bc30ad29e3060d10a8c43513a37a2ebb
Rosnec <rosnec.dev@gmail.com>
2014-02-16 18:45:38 -0500

    overriding socket's accept method to output a MySocket object

--- mysocket.py
+++ mysocket.py
@@ -4,6 +4,22 @@ class MySocket(socket.socket):
     def __init__(self, **kwargs):
         super(MySocket, self).__init__(**kwargs)
 
+    def accept(self):
+        """accept() -> (socket object, address info)
+
+        Wait for an incoming connection.  Return a new socket
+        representing the connection, and the address of the client.
+        For IP sockets, the address info is a pair (hostaddr, port).
+        """
+        fd, addr = self._accept()
+        sock = MySocket(self.family, self.type, self.proto, fileno=fd)
+        # Issue #7995: if no default timeout is set and the listening
+        # socket had a (non-zero) timeout, force the new socket in blocking
+        # mode to override platform-specific socket flags inheritance.
+        if getdefaulttimeout() is None and self.gettimeout():
+            sock.setblocking(True)
+        return sock, addr
+        
     def sendby(self, msg, msgsize, bufsize):
         """Sends an entire message in chunks of size bufsize"""
         bufsize += 1 # account for string slicing being end-exclusive
