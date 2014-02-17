34137eb654d335f98f6085f27030565a18c72cbf
Rosnec <rosnec.dev@gmail.com>
2014-02-16 19:03:34 -0500

    improving client functionality

--- client.py
+++ client.py
@@ -13,20 +13,24 @@ parser.add_argument('host', metavar='H',
                     help='Set host to connect to.')
 parser.add_argument('port', metavar='P', type=int,
                     help='Set port to use.')
-parser.add_argument('msgsize', metavar='MS', type=int,
-                    help='Set log_2 size of message.')
-parser.add_argument('bufsize', metavar='BS', type=int,
-                    help='Set log_2 size of buffers.')
+
 
 
 args = parser.parse_args()
 
-choice_map = {'TCP' : {'RTT'        : roundtripclient.TCP,
-                       'throughput' : throughputclient.TCP,
-                       'size'       : sizeclient.TCP},
-              'UDP' : {'RTT'        : roundtripclient.UDP,
-                       'throughput' : throughputclient.UDP,
-                       'size'       : sizeclient.UDP}}
+RTT_sizes = (0, 4, 8)
+throughput_sizes = (10, 12, 14, 16, 18, 20)
+size_sizes = (16, 17, 18, 19, 20)
 
-print(choice_map[args.type][args.mode](args.host, args.port,
-                                       args.msgsize, args.bufsize))
+choice_map = {'TCP' : {'RTT'        : (roundtripclient.TCP, RTT_sizes),
+                       'throughput' : (throughputclient.TCP, throughput_sizes),
+                       'size'       : (sizeclient.TCP}, size_sizes)},
+              'UDP' : {'RTT'        : (roundtripclient.UDP, RTT_sizes),
+                       'throughput' : (throughputclient.UDP, throughput_sizes),
+                       'size'       : (sizeclient.UDP, size_sizes)}}
+
+func, lst = choice_map[args.type][args.mode]
+for n in lst:
+    size = 2**n
+    print("2^{}B in {}s".format(n, func(args.host, args.port,
+                                        size, size)))
