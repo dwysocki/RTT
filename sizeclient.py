cefa6d6ea163bd7ba446b36f3a242e1e6da59fad
Rosnec <rosnec.dev@gmail.com>
2014-02-18 20:22:21 -0500

    fixin things and stuff

--- sizeclient.py
+++ sizeclient.py
@@ -5,5 +5,11 @@ import mysocket
 def TCP(host, port, msgsize, bufsize):
     print("TCP size client not yet implemented")
 
+def test_TCP(host, port, size):
+    return TCP(host, port, size, size)
+
 def UDP(host, port, msgsize, bufsize):
     print("UDP size client not yet implemented")
+
+def test_UDP(host, port, size):
+    return UDP(host, port, size, size)
