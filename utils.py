659840965ad79a6d4a60098427674a513784dacd
Rosnec <rosnec.dev@gmail.com>
2014-02-26 08:24:57 -0500

    debugging

--- utils.py
+++ utils.py
@@ -1,19 +1,18 @@
 import socket
 
 def mod256(x):
-    """Finds x modulus 256"""
+    """Finds `x` modulus 256"""
     return x % 256
 
 def makebytes(n):
-    """Makes a byte object with n bytes. Each successive byte is the increment
+    """Makes a byte object with `n` bytes. Each successive byte is the increment
     of the last, modulo 256"""
     return bytes(map(mod256,
                      range(n)))
 
-def get_bit(decimal, N):
-    """Returns the `N`th bit from `decimal`"""
-    constant = 1 << N
-    return decimal & constant
+def get_bit(number, N):
+    """Returns whether the `N`th bit from `number` is on or off."""
+    return bool(number & (1 << N))
 
 type_map = {'TCP' : socket.SOCK_STREAM,
             'UDP' : socket.SOCK_DGRAM}
