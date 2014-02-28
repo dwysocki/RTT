794d6ba2056b27a49d7b1a019536bf3d568fde16
Rosnec <rosnec.dev@gmail.com>
2014-02-27 21:29:27 -0500

    fixing logical error

--- mysocket.py
+++ mysocket.py
@@ -9,6 +9,7 @@ import utils
 
 # special bytes for sending messages
 ACK, NACK, MODE_ROUNDTRIP, MODE_THROUGHPUT, MODE_SIZES = range(25, 100, 15)
+ACK, NACK = map(lambda B: bytes(list(B)), (ACK, NACK))
 # most efficient UDP datagram size
 datagram_size = 2**13
 
